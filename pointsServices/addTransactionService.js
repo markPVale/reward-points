6d3f962bacee26d6b9783cd60bf433c8323c5e44
Mark-Vale <markpvale@gmail.com>
2021-12-09 22:32:39 -0500

    clean up variables and delete extra comments

--- pointsServices/addTransactionService.js
+++ pointsServices/addTransactionService.js
@@ -9,40 +9,36 @@ exports.addTransactionService = (transactionData, timestamp) => {
 
   if (users.length === 0) {
     addUserToDB(new User(userId, points, payer));
-    userCheck.add(userId)
+    userCheck.add(userId);
     users[0].transactions.push(transactionData);
-    users[0].payerBalances.set(payer, points)
-    // console.log('added 0 length', 'userId: ', userId, "**", util.inspect(users, false, null, true));
+    users[0].payerBalances.set(payer, points);
   } else if (!userCheck.has(userId)) {
     addUserToDB(new User(userId, points, payer))
-    userCheck.add(userId)
+    userCheck.add(userId);
     users[0].transactions.push(transactionData);
-    users[0].payerBalances.set(payer, points)
-    // console.log('added non 0 length', 'userId: ', userId, "**", util.inspect(users, false, null, true));
+    users[0].payerBalances.set(payer, points);
   } else {
     for (let user of users) {
       if (user.userId === transactionData.userId) {
         if(user.totalPoints + points >= 0) {
-          user.totalPoints += points
+          user.totalPoints += points;
           user.transactions.push(transactionData);
-          sortByTimestamp(user.transactions)
+          sortByTimestamp(user.transactions);
           if (user.payerBalances.has(transactionData.payer)) {
-            let currentBalance = user.payerBalances.get(payer)
+            let currentBalance = user.payerBalances.get(payer);
             if (currentBalance + points >= 0) {
               let updateBalance = user.payerBalances.get(payer) + points;
               user.payerBalances.set(payer, updateBalance);
-              // console.log('added already in update payer', util.inspect(users, false, null, true));
-            }
+            };
           } else if (points > 0) {
-            user.payerBalances.set(payer, points)
-            // console.log('added already in new payer', util.inspect(users, false, null, true));
+            user.payerBalances.set(payer, points);
           } else {
-            console.log('Error: Attempt To Update Negative Points From A Zero Balance')
+            console.log('Error: Attempt To Update Negative Points From A Zero Balance');
           }
-          break
+          break;
         } else {
-          console.log('Invalid number of points')
-          break
+          console.log('Invalid number of points');
+          break;
         }
       }
     }
